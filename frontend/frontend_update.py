020d973f70b6073afcdc88c2095639bc51f35e74
Elan456 <telan4892@gmail.com>
2025-03-08 01:08:59 -0500

    Add current task to FRU

--- frontend/frontend_update.py
+++ frontend/frontend_update.py
@@ -3,9 +3,11 @@ Contain all the important things that can get changed from the frontend
 """
 
 from llm.chat import Chat 
+from interview_master.task import Task
 
 class FrontendUpdate:
-    def __init__(self, chat: Chat, code: str, code_output: str):
+    def __init__(self, chat: Chat, code: str, code_output: str, current_task: Task):
         self.chat = chat
         self.code = code
         self.code_output = code_output
+        self.current_task = current_task
